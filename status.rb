539e8cf6d91bc764e767ee5115677546c684901f
Steve Nester <steve@sahfor.com>
2015-03-11 07:16:14 -0700

    Fixed status.rb issue

--- status.rb
+++ status.rb
@@ -5,14 +5,15 @@ require 'uri'
 require 'json'
  
 def getPaymentStatus(checkoutId)
-	uri = URI.parse("https://test.oppwa.com/v1/checkouts/" + checkoutId + "/payment")
-	puts "GET request to: " + uri.path
-	res = Net::HTTP.get_response(uri)
-	puts res.body
+	uri = URI("https://test.oppwa.com/v1/checkouts/" + checkoutId + "/payment")
+	http = Net::HTTP.new(uri.host, uri.port)
+	http.use_ssl = true
+	req = Net::HTTP::Get.new(uri.path)
+	res = http.request(req)
 	return JSON.parse(res.body)
 end
 
-status = getPaymentStatus("657C5266C2EFE56D32B20F5CD07F1221.sbg-vm-tx02")
+status = getPaymentStatus("D17CCA478699234A362282C97C5C1589.sbg-vm-tx02")
 if (status["result"]["code"].start_with?("000"))
 	puts "SUCCESS <br/><br/> Here is the result of your transaction: <br/><br/>"
 	puts status
